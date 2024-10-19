0b69e88736751839a9e4779e778a5b2bcc54820e
Manuel Nicolás <mediavilla.grosjean@gmail.com>
2024-10-19 18:55:14 -0300

    Agregar importacion manejador de errores al paquete logger

--- src/logger/manejador_logs.py
+++ src/logger/manejador_logs.py
@@ -3,14 +3,14 @@ import logging
 logger = logging.getLogger('actividad')
 logger.setLevel(logging.INFO)
 
-handler = logging.FileHandler('logs/actividad.txt')
+handler = logging.FileHandler('../logs/actividad.txt')
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 def registrar_actividad(mensaje, resultado=None):
     if resultado:
-        mensaje_completo = f"{mensaje}. Resultado: {resultado}"
+        mensaje_completo = f"{mensaje} Resultado: {resultado}"
     else:
         mensaje_completo = mensaje
     logger.info(mensaje_completo)
